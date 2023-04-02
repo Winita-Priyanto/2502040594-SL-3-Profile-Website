5becfdff571f9aaf222d6b1d0ee7dc33b5d722de
Winita-Priyanto <winita.priyanto@binus.ac.id>
2023-04-02 21:25:09 +0700

    first commit

---
+++ app/Http/Controllers/Profile_Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Profile_Controller extends Controller
+{
+    function index() {
+        return view('profile.index');
+    }
+    function update() {
+
+    }
+}
